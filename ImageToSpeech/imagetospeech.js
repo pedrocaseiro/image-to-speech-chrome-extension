27b38a26c33d0df2e2914203947bbfe6e8052663
pedrocaseiro <pcaseiro96@gmail.com>
2017-02-19 11:36:18 +0000

    code cleanup

--- ImageToSpeech/imagetospeech.js
+++ ImageToSpeech/imagetospeech.js
@@ -1,72 +1,34 @@
-
 var anchors = document.getElementsByTagName("a");
-var images = document.getElementsByTagName("img");
 
 for (var i = 0; i < anchors.length; i++){
   anchors[i].addEventListener("focus", myFunction);
-  }
-
-
- for (var j = 0; j < images.length; j++){
-  images[j].addEventListener("focus", myFunctionImg);
-  }
-
-
-
-//chrome.contextMenus.create({
-//title: "Search: %s", 
-//contexts:["page", "selection"], 
-//onclick: clickHandler,
-//});
-function myFunction(e){
-	console.log("a");
-	var imgSrcVal = $(e.target).find("img").attr("src");
-	
-	if(imgSrcVal != undefined) {
-		
-		console.log("ddd");
-		// POST AO SERVER
-		httpPOST(imgSrcVal);
-		//window.speechSynthesis.speak(msg);
-
-	}
 }
 
-function myFunctionImg(e){
-	console.log("img");
-	var imgSrcVal = $(e.target).attr("src");
+function myFunction(e){
+  console.log("a");
+  var imgSrcVal = $(e.target).find("img").attr("src");
 
-
-	if(imgSrcVal != undefined) {
-		console.log(imgSrcVal);
-		// POST AO SERVER
-		httpPOST(imgSrcVal);
-		//window.speechSynthesis.speak(msg);
-
-	}
+  if(imgSrcVal != undefined) {
+    httpPOST(imgSrcVal);
+  }
 }
 
 
 function httpPOST(data){       
-	var http = new XMLHttpRequest();
-	console.log(data);
-	var url = "https://0.0.0.0:5000/getDescription";
-	var params = "data="+data;
-	http.open("POST", url, true);
+  var http = new XMLHttpRequest();
+  var url = "https://kzpree.me:5000/getDescription";
+  var params = "data="+data;
+  http.open("POST", url, true);
 
-	//Send the proper header information along with the request
-	//http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-	http.onreadystatechange = function() {//Call a function when the state changes.
-		console.log(http.status);
-	    if(http.readyState == 4 && http.status == 200) {
-	    	var r = JSON.parse(http.responseText);
-	        console.log(r["success"]);
-	        window.speechSynthesis.speak(new SpeechSynthesisUtterance(r["success"]));
-	    }
-	}
-	http.send(params);
-
+  http.onreadystatechange = function() {//Call a function when the state changes.
+    console.log(http.status);
+    if(http.readyState == 4 && http.status == 200) {
+      var r = JSON.parse(http.responseText);
+      console.log(r["success"]);
+      window.speechSynthesis.speak(new SpeechSynthesisUtterance(r["success"]));
+    }
+  }
+  http.send(params);
 }
 
 
