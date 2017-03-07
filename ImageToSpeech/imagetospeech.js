485dd3f20f6dd02c5a3e8b385f58b543206d6c76
pedrocaseiro <pcaseiro96@gmail.com>
2017-03-07 10:35:31 +0000

    changes the on scroll loader from the extension

--- ImageToSpeech/imagetospeech.js
+++ ImageToSpeech/imagetospeech.js
@@ -1,12 +1,9 @@
-document.addEventListener('scroll', function(){
-  var anchors = document.getElementsByTagName("a");
-  for (var i = 0; i < anchors.length; i++){
-    anchors[i].addEventListener("focus", myFunction);
-  }
-}, false);
+var anchors = document.getElementsByTagName("a");
+for (var i = 0; i < anchors.length; i++){
+  anchors[i].addEventListener("focus", focusTrigger);
+}
 
-function myFunction(e){
-  console.log("a");
+function focusTrigger(e){
   var imgSrcVal = $(e.target).find("img").attr("src");
 
   if(imgSrcVal != undefined) {
@@ -22,10 +19,8 @@ function httpPOST(data){
   http.open("POST", url, true);
 
   http.onreadystatechange = function() {//Call a function when the state changes.
-    console.log(http.status);
     if(http.readyState == 4 && http.status == 200) {
       var r = JSON.parse(http.responseText);
-      console.log(r["success"]);
       window.speechSynthesis.speak(new SpeechSynthesisUtterance(r["success"]));
     }
   }
